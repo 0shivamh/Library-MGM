ea62c3d93083ecc848a2814a065425f3752dd476
Shivam Hande <shivamhande0000@gmail.com>
2023-02-17 19:32:09 +0530

    add: state

--- src/pages/signUp.page.jsx
+++ src/pages/signUp.page.jsx
@@ -14,19 +14,29 @@ const SignUpPage=()=>{
         <div className="d-flex justify-content-center m-4">
             <Form className="text-center">
                 <FloatingLabel controlId="floatingPassword" label="Librarian Id">
-                    <Form.Control type="text" placeholder="name@cloudredux.com"  />
+                    <Form.Control type="text" placeholder="name@cloudredux.com"
+                                  value={libId}
+                                  onChange={(e)=> setLibId(e.target.value)}/>
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPassword" label="Full Name">
-                    <Form.Control type="text" placeholder="name@cloudredux.com"  />
+                    <Form.Control type="text" placeholder="name@cloudredux.com"
+                                  value={name}
+                                  onChange={(e)=> setName(e.target.value)}/>
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPassword" label="Email Address">
-                    <Form.Control type="email" placeholder="name@cloudredux.com" />
+                    <Form.Control type="email" placeholder="name@cloudredux.com"
+                                  value={email}
+                                  onChange={(e)=> setEmail(e.target.value)}/>
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPassword" label="Mobile Number">
-                    <Form.Control type="number" placeholder="name@cloudredux.com"  />
+                    <Form.Control type="number" placeholder="name@cloudredux.com"
+                                  value={phone}
+                                  onChange={(e)=> setPhone(e.target.value)}/>
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPassword" label="Password">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password"
+                                  value={psw}
+                                  onChange={(e)=> setPsw(e.target.value)}/>
                 </FloatingLabel>
                 <Button className="white-btn mt-2 mb-2" type="submit">
                     Sign Up
--- src/pages/signin.page.jsx
+++ src/pages/signin.page.jsx
@@ -1,6 +1,11 @@
 import {Button, Container, FloatingLabel, Form} from "react-bootstrap";
+import {useState} from "react";
 
 const SignInPage=()=>{
+
+    const [email,setEmail] = useState();
+    const [psw,setPsw] = useState();
+
     return(<>
         <div className="d-flex justify-content-center m-4">
             <Form className="text-center">
@@ -9,10 +14,14 @@ const SignInPage=()=>{
                     label="Email address"
                     className="mb-3"
                 >
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Control type="email" placeholder="name@example.com"
+                                  value={email}
+                                  onChange={(e)=> setEmail(e.target.value)} />
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPassword" label="Password">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password"
+                                  value={psw}
+                                  onChange={(e)=> setPsw(e.target.value)}/>
                 </FloatingLabel>
                 <Button className="white-btn mt-2 mb-2" type="submit">
                     Sign In
